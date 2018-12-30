853ed98802493c1188253668c74af449a821ea83
Hichu Yamichu <HichuYamichu@gmail.com>
2018-12-30 15:29:33 +0100

    Add project files.

---
+++ files/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter path for new file");
+            string pathForFile = Console.ReadLine();
+            Console.WriteLine("Enter directory path");
+            string directoryPath = Console.ReadLine();
+
+            try
+            {
+                StreamWriter sw = File.CreateText(pathForFile + @"\output.txt");
+                sw.Close();
+
+                string[] filePaths = Directory.GetFiles(@directoryPath);
+
+                foreach (var file in filePaths)
+                {
+                    Console.WriteLine(file);
+
+                    List<string> lines = File.ReadAllLines(file).ToList();
+
+                    var tw = new StreamWriter(pathForFile + @"\output.txt", true);
+                    tw.WriteLine(file + Environment.NewLine);
+
+                    foreach (string line in lines)
+                    {
+                        tw.WriteLine(line);
+                        Console.WriteLine(line);
+                    }
+
+                    tw.WriteLine(Environment.NewLine);
+                    tw.Close();
+
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
